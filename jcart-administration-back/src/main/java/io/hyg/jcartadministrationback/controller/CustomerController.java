4ef24d50b1b4c42d12ab17a96a9a69c23fc06058
HUYINGE\28172 <h20001005@163.com>
2020-02-25 16:40:15 +0800

    draft customer apis

---
+++ jcart-administration-back/src/main/java/io/hyg/jcartadministrationback/controller/CustomerController.java
@@ -0,0 +1,39 @@
+package io.hyg.jcartadministrationback.controller;
+
+import io.hyg.jcartadministrationback.dto.in.CustomerSearchInDTO;
+import io.hyg.jcartadministrationback.dto.out.CustomerListOutDTO;
+import io.hyg.jcartadministrationback.dto.out.CustomerShowOutDTO;
+import io.hyg.jcartadministrationback.dto.out.PageOutDTO;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+
+    @GetMapping("/search")
+    public PageOutDTO<CustomerListOutDTO> search(CustomerSearchInDTO customerSearchInDTO,
+                                                 @RequestParam Integer pageNum){
+        return null;
+    }
+
+//回显
+    @GetMapping("/getById")
+    public CustomerShowOutDTO getById(@RequestParam Integer customerId){
+        return null;
+    }
+
+
+    //创建和添加
+    @PostMapping("/disable")
+    public void disable(@RequestParam Integer customerId){
+
+    }
+
+    
+
+
+
+
+
+}
