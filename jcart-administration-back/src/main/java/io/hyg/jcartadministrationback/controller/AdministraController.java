108685fc80d41e740419c4bfd690101b30e7f06c
HUYINGE\28172 <h20001005@163.com>
2020-02-25 16:12:12 +0800

    draft done administrator api

--- jcart-administration-back/src/main/java/io/hyg/jcartadministrationback/controller/AdministraController.java
+++ jcart-administration-back/src/main/java/io/hyg/jcartadministrationback/controller/AdministraController.java
@@ -1,10 +1,9 @@
 package io.hyg.jcartadministrationback.controller;
 
-import io.hyg.jcartadministrationback.dto.in.AdministratorLoginInDTO;
-import io.hyg.jcartadministrationback.dto.in.AdministratorResetPwdInDTO;
-import io.hyg.jcartadministrationback.dto.in.AdministratorUpdateProfileInDTO;
+import io.hyg.jcartadministrationback.dto.in.*;
 import io.hyg.jcartadministrationback.dto.out.AdministratorGetProfileOutDTO;
 import io.hyg.jcartadministrationback.dto.out.AdministratorListOutDTO;
+import io.hyg.jcartadministrationback.dto.out.AdministratorShowOutDTO;
 import io.hyg.jcartadministrationback.dto.out.PageOutDTO;
 import org.springframework.web.bind.annotation.*;
 
@@ -45,6 +44,23 @@ public class AdministraController {
         return null;
     }
 
-    
+    //回显的接口
+    @GetMapping("/getById")
+    public AdministratorShowOutDTO getById(@RequestParam Integer administratorId){
+        return null;
+    }
+
+    //管理员添加页
+    @PostMapping("/create")
+    public Integer create(@RequestBody AdministratorCreateInDTO administratorCreateInDTO){
+        return null;
+    }
+
+
+    //修改数据库
+    @PostMapping("/update")
+    public void update(@RequestBody AdministratorUpdateInDTO administratorUpdateInDTO){
+
+    }
 
 }
