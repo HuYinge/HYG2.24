ac3579ef8a475639193582d6d0ecee87d81cd140
HUYINGE\28172 <h20001005@163.com>
2020-02-25 16:48:07 +0800

    add address api

---
+++ jcart-administration-back/src/main/java/io/hyg/jcartadministrationback/controller/AddressController.java
@@ -0,0 +1,22 @@
+package io.hyg.jcartadministrationback.controller;
+
+import io.hyg.jcartadministrationback.dto.out.AddressListOutDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/address")
+public class AddressController {
+
+    @GetMapping("/getListByCustomerId")
+    public List<AddressListOutDTO> getListByCustomerId(@RequestParam Integer customerId){
+        return null;
+    }
+
+
+
+}
