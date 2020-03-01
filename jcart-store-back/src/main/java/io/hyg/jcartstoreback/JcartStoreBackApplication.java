dd9d9c69d3cb49e6ad0363131ffe8c1d9ffea8d3
HUYINGE\28172 <h20001005@163.com>
2020-03-01 19:02:07 +0800

    no message

--- jcart-store-back/src/main/java/io/hyg/jcartstoreback/JcartStoreBackApplication.java
+++ jcart-store-back/src/main/java/io/hyg/jcartstoreback/JcartStoreBackApplication.java
@@ -1,9 +1,11 @@
 package io.hyg.jcartstoreback;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("io.hyg.jcartstoreback.dao")
 public class JcartStoreBackApplication {
 
     public static void main(String[] args) {
--- jcart-store-back/src/main/java/io/hyg/jcartstoreback/util/JWTUtil.java
+++ jcart-store-back/src/main/java/io/hyg/jcartstoreback/util/JWTUtil.java
@@ -4,6 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
+
 import io.hyg.jcartstoreback.dto.out.CustomerLoginOutDTO;
 import io.hyg.jcartstoreback.po.Customer;
 import io.hyg.jcartstoreback.vo.CustomerLoginVO;
