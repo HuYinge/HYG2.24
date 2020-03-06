e65cbd7df511402ddfcf2ea5b68fc09b9fa9c20d
HUYINGE\28172 <h20001005@163.com>
2020-03-06 20:51:39 +0800

    add administrator customer search page

--- jcart-administration-back/src/main/java/io/hyg/jcartadministrationback/controller/AddressController.java
+++ jcart-administration-back/src/main/java/io/hyg/jcartadministrationback/controller/AddressController.java
@@ -5,16 +5,15 @@ import io.hyg.jcartadministrationback.dto.out.AddressShowOutDTO;
 import io.hyg.jcartadministrationback.po.Address;
 import io.hyg.jcartadministrationback.service.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/address")
+@CrossOrigin
 public class AddressController {
 
     @Autowired
