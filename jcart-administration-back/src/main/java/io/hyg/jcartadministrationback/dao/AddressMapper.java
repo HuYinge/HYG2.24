70e2fc5e9778d4eecc1c850fc7b36646a9e11f9f
HUYINGE\28172 <h20001005@163.com>
2020-03-06 19:15:34 +0800

    add administration address get list by customerId api

--- jcart-administration-back/src/main/java/io/hyg/jcartadministrationback/dao/AddressMapper.java
+++ jcart-administration-back/src/main/java/io/hyg/jcartadministrationback/dao/AddressMapper.java
@@ -2,7 +2,10 @@ package io.hyg.jcartadministrationback.dao;
 
 
 import io.hyg.jcartadministrationback.po.Address;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AddressMapper {
@@ -17,4 +20,7 @@ public interface AddressMapper {
     int updateByPrimaryKeySelective(Address record);
 
     int updateByPrimaryKey(Address record);
+
+    //custom
+    List<Address> selectByCustomerId(@Param("customerId") Integer customerId);
 }
--- jcart-administration-back/src/main/java/io/hyg/jcartadministrationback/service/AddressService.java
+++ jcart-administration-back/src/main/java/io/hyg/jcartadministrationback/service/AddressService.java
@@ -2,7 +2,11 @@ package io.hyg.jcartadministrationback.service;
 
 import io.hyg.jcartadministrationback.po.Address;
 
+import java.util.List;
+
 public interface AddressService {
 
     Address getById(Integer addressId);
+
+    List<Address> getByCustomerId(Integer customerId);
 }
